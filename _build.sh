36add7c2c5ba8ac3b0193955c7987b82751707dd
Chris Diaz <chris-diaz@northwestern.edu>
2019-08-08 11:01:18 -0500

    travis-ci setup

--- _build.sh
+++ _build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-Rscript -e "bookdown::clean_book(TRUE); bookdown::render_book('index.Rmd', 'bookdown::gitbook');"
-Rscript -e "source('purl.R')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook');"
 
