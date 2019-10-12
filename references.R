7a39a0d0c269f1a5b9b97ed460a415560bb8b7ba
kgfitzgerald <kgfitzgerald@u.northwestern.edu>
2019-10-12 16:14:10 -0500

    commented out broken references link ch 6

--- references.R
+++ references.R
@@ -1,2 +1,5 @@
 #05-regression.Rmd
-[@R-tidyverse] #Needed packages
+[@R-tidyverse] #Needed packages section
+
+#06-multiple-regression.Rmd
+[@islr2017] #two numerical explanatory variables (section 6.1)
